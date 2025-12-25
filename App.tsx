bd86b581d9bfe9f296d2f4e38b0d20ddc2bd115e
krisonuseng-ship-it <krisonuseng@gmail.com>
2025-12-25 15:07:58 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -183,7 +183,7 @@ const translations = {
   }
 };
 
-const API_URL = 'http://agri-app-kih4.onrender.com/api';
+const API_URL = 'https://agri-app-kih4.onrender.com/api';
 
 const App: React.FC = () => {
   // --- State ---
