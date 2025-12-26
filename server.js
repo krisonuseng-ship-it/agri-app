b52e6d0a68209c9ce450df327f1c56246fa7c270
krisonuseng-ship-it <krisonuseng@gmail.com>
2025-12-26 13:49:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 const SECRET_KEY = "my_super_secret_key_change_this"; // กุญแจลับสำหรับ Login
 
 // ตั้งค่า AI (อย่าลืมใส่ GEMINI_API_KEY ในไฟล์ .env ของคุณนะ!)
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI("AIzaSyBgmxL_-TECOXBLJTK5bRNvi4trVtEZSz4");
 
 // Middleware
 app.use(cors());
